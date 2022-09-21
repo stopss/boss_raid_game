3cf5a0e1ed50fbbc66ccc0ecdbce9a3ef99c6092
jieun <stopss417@gmail.com>
2022-09-21 15:45:39 +0900

    feat: 보스레이드 상태조회 API 개발 (#5)

--- src/api/bossraid/bossraid.controller.ts
+++ src/api/bossraid/bossraid.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BossraidService } from './bossraid.service';
 import { BossraidStartDto } from './dto/bossraid.start.dto';
 
@@ -9,5 +9,10 @@ export class BossraidController {
   @Post('enter')
   start(@Body() bossraidStartDto: BossraidStartDto): Promise<any> {
     return this.bossRaidService.start(bossraidStartDto);
+  }
+
+  @Get()
+  status(): Promise<any> {
+    return this.bossRaidService.status();
   }
 }
--- src/api/bossraid/bossraid.service.ts
+++ src/api/bossraid/bossraid.service.ts
@@ -55,4 +55,60 @@ export class BossraidService {
       throw NotFoundException;
     }
   }
+
+  /**
+   * 보스레이드 상태조회
+   * @returns {boolean} canEnter - 입장 가능한지
+   * @returns {boolean} enteredUserId - 입장한 유저의 id
+   */
+  async status(): Promise<any> {
+    // 보스레이드 시작한 기록이 있는지 확인
+    const raidRecord = await this.raidRecordRepository.find({
+      where: { status: true },
+    });
+
+    // 보스레이드 시작한 기록이 없다면
+    if (raidRecord.length === 0) {
+      return Object.assign({
+        success: true,
+        statusCode: 200,
+        data: { canEnter: true },
+        message: '보스레이드를 입장 가능합니다.',
+        timestamp: new Date().toISOString(),
+      });
+    }
+
+    // 보스레이드 시작한 기록이 있다면
+    // 시작한 시간으로부터 레이드 경과시간(3분)만큼 경과되어야 함
+    const enterTime = raidRecord[0].enterTime.getTime();
+    const date = new Date().getTime();
+    const elapsedTime = (date - enterTime) / 1000;
+
+    // 시간(3분)이 경과 되지 않았다면
+    if (elapsedTime < 180) {
+      return Object.assign({
+        success: false,
+        statusCode: 200,
+        data: { canEnter: false, enteredUserId: raidRecord[0].enteredUserId },
+        message: '보스레이드를 입장 불가능합니다.',
+        timestamp: new Date().toISOString(),
+      });
+    }
+
+    // 시간(3분)이 경과되었다면
+    await this.raidRecordRepository.update(
+      {
+        raidRecordId: raidRecord[0].raidRecordId,
+      },
+      { status: false },
+    );
+    
+    return Object.assign({
+      success: true,
+      statusCode: 200,
+      data: { canEnter: true},
+      message: '보스레이드를 입장 가능합니다.',
+      timestamp: new Date().toISOString(),
+    });
+  }
 }
